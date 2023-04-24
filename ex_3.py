572a33756201f33e6cc8560039028bc26d497f91
Jan1029 <jan.galazka2108@gmail.com>
2023-04-24 12:22:59 +0200

    zad3

--- ex_3.py
+++ ex_3.py
@@ -1,5 +1,12 @@
 # Policz studentki i studentow i zwroc wynik w formacie: [k, m]
 # Przyjmij, ze imie dla kobiety konczy sie na "a"
 def policz_studentow_plec(studenci) -> [int, int]:
-    # TODO
-    return [0, 0]
+    k = 0
+    m = 0
+    for student in studenci:
+        if student.split()[0].endswith("a"):
+            k += 1
+        else:
+            m += 1
+
+    return [k, m]
